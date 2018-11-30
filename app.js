c40c373d1169388777fcc88e59934e8b5e47704d
saloni1990 <salonicmd@gmail.com>
2018-11-30 10:52:34 +0000

    update server

--- app.js
+++ app.js
@@ -53,12 +53,9 @@ app.use("/blog/:id/comments",commentsRoute);
 app.use("/blog", blogRoute); 
 app.use("/", landingRoute); 
 
-    var port = process.env.PORT || 3000;
-
-    var server = app.listen(port, function () {
-        console.log('Server running at http://127.0.0.1:' + port + '/');
-    });
-
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Server Has Started!");
+ });
 
 // app.listen(3000, function(){
 //     console.log("Server is running");
